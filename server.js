1274378b9428d165f05376db9508e89b353a4160
ibrahim <bobsamak@gmail.com>
2018-11-22 18:26:11 +0200

    db1

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'dist')));
 
 
-mongoose.connect('mongodb://localhost:27017/liken' , { useNewUrlParser: true })
+mongoose.connect(config.get('db_liken') , { useNewUrlParser: true })
 .then(()=>('connect to db'))
 .catch(()=>('err'))
 
